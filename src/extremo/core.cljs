0102f4df4f53b2bb927a2e88e587e9f3e7c260d0
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2016-12-03 15:10:18 +0100

    Initial commit

---
+++ src/extremo/core.cljs
@@ -0,0 +1,38 @@
+(ns extremo.core
+  (:require
+    [extremo.routes :refer [router]]
+    [macchiato.env :as config]
+    [macchiato.http :refer [handler]]
+    [macchiato.session.memory :as mem]
+    [mount.core :as mount :refer [defstate]]
+    [taoensso.timbre :refer-macros [log trace debug info warn error fatal]]))
+
+(defstate env :start (config/env))
+
+(defstate http :start (js/require "http"))
+
+(defn app []
+  (mount/start)
+  (let [host (or (:host env) "127.0.0.1")
+        port (or (some-> env :port js/parseInt) 3000)]
+    (-> @http
+        (.createServer
+          (handler
+            router
+            {:cookies {:signed? true}
+             :session {:store (mem/memory-store)}}))
+        (.listen port host #(info "extremo started on" host ":" port)))))
+
+(defn start-workers [os cluster]
+  (dotimes [_ (-> os .cpus .-length)]
+    (.fork cluster))
+  (.on cluster "exit"
+       (fn [worker code signal]
+         (info "worker terminated" (-> worker .-process .-pid)))))
+
+(defn main [& args]
+  (let [os      (js/require "os")
+        cluster (js/require "cluster")]
+    (if (.-isMaster cluster)
+      (start-workers os cluster)
+      (app))))
---
+++ src/extremo/routes.cljs
@@ -0,0 +1,35 @@
+(ns extremo.routes
+  (:require
+    [bidi.bidi :as bidi]
+    [hiccups.runtime]
+    [macchiato.response :as r])
+  (:require-macros
+    [hiccups.core :refer [html]]))
+
+(defn home [req res]
+  (-> (html
+        [:html
+         [:body
+          [:h2 "Hello World!"]
+          [:p
+           "Your user-agent is: "
+           (str (-> req :headers :user-agent))]]])
+      (r/ok)
+      (res)))
+
+(defn not-found [req res]
+  (-> (html
+        [:html
+         [:body
+          [:h2 (:uri req) " was not found"]]])
+      (r/not-found)
+      (res)))
+
+(def routes
+  ["/"
+   [["" home]
+    [true not-found]]])
+
+(defn router [req res]
+  (let [route (->> req :uri (bidi/match-route routes) :handler)]
+    (route req res)))
